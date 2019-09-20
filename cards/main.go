4ec13e771b2336b976172b7b5f3ff71ba9d008ad
Ken <kbsizer@gmail.com>
2019-09-20 11:26:18 -0400

    reversing strings; methods on non-struct types

--- cards/main.go
+++ cards/main.go
@@ -9,6 +9,16 @@ func main() {
 	card := newCard()
 	fmt.Println(card)
 	fmt.Println(deckSize)
+
+	// a slice of type string
+	cards := []string{"Ace of Spades", "Jack of Diamonds", "3 of Clubs"}
+	fmt.Println("cards =", cards)
+	fmt.Printf("cards = %v\n", cards)
+	// LESSON 16
+
+	r := reversibleString("1234567890 The quick brown 狐 jumped over the lazy 犬")
+	fmt.Println("r           = ", r)
+	fmt.Println("r.Reverse() = ", r.Reverse())
 }
 
 func newCard() string {
@@ -17,4 +27,15 @@ func newCard() string {
 
 func estPi() float64 {
 	return 3.14
+}
+
+//my attempt at a method
+type reversibleString string
+
+func (s reversibleString) Reverse() string {
+	runes := []rune(string(s))
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
